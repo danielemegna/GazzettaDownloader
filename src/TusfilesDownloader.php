4850d15211050ddceaf3d1d53467aef2d612b798
Daniele Megna <megna.dany@gmail.com>
2014-12-21 13:23:31 +0100

    Tusdownloader test passed with fake file touch

--- src/TusfilesDownloader.php
+++ src/TusfilesDownloader.php
@@ -2,9 +2,16 @@
 
 class TusfilesDownloader
 {
+  private $curlHelper;
+
+  function __construct($curlHelper)
+  {
+    $this->curlHelper = $curlHelper;
+  }
+
   function download($link, $filepath)
   {
-    
+    $this->curlHelper->getSource($link);
     exec("touch $filepath"); 
   }
 }
--- tests/TusfilesDownloaderTest.php
+++ tests/TusfilesDownloaderTest.php
@@ -2,17 +2,26 @@
 
 class TusfilesDownloaderTest extends PHPUnit_Framework_TestCase
 {
+  private $td;
+
+  function setUp()
+  {
+    $this->td = new TusfilesDownloader(
+      new CurlHelper()
+    );
+  }
 
   function testTDCanDownloadFromLink()
   {
-    $td = new TusfilesDownloader();
     $downloadLink = "https://www.tusfiles.net/jw1zzzh3eu34";
 
     $filepath = "/home/daniele/Dropbox/homeip/GazzettaTest.pdf";
-    $td->download($downloadLink, $filepath);
+    $this->td->download($downloadLink, $filepath);
 
     $this->assertFileExists($filepath);
     exec("rm $filepath");
+
+    // http://p.tusfiles.net/d/a4k3rch5tz2fvxijr7zzjfqgcb3zsdsih6dataipb62qpuumpmp7kgjd/GDS(2014_12_03).pdf
   }
 
 }
