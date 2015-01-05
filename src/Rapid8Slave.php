ebec0b5492769502157832e4b420033bdd5875a4
Daniele Megna <megna.dany@gmail.com>
2015-01-05 10:41:44 +0100

    Developed rapid8 test and provider recognize

--- src/Rapid8Slave.php
+++ src/Rapid8Slave.php
@@ -16,12 +16,40 @@ class Rapid8Slave
     $this->supportedProviders = ["1Fichier", "2Shared", "Dailymotion", "Depfile", "Filefactory", "Filepost", "FileRio", "Filesflash", "Hugefiles.net", "Kingfiles.net", "Mixturecloud", "Nowdownload.ch", "Rapidgator.net", "Sendspace", "Share-Online.biz", "Speedyshare", "Uploadable", "Uploaded.net", "Uploading", "Uploadto.us", "Uptobox", "YouTube", "Zippyshare", "Asfile", "Clicknupload", "Extmatrix", "Fboom.me", "Filetut", "Lafiles", "Megairon.net", "vimeo.com"];
   }
   
-  function isProviderSupported($link)
+  function isLinkSupported($link)
   {
+    $link = strtolower($link);
+    foreach($this->supportedProviders as $provider) {
+      if(strpos($link,strtolower($provider)) !== false)
+        return true;
+    }
+
     return false; 
   }
 
   function work($linkToDownload)
+  {
+    $this->sendMessageToBox($linkToDownload);
+
+    sleep(self::SECOND_TO_WAIT_FOR_RESPONSE);
+
+    return $this->getReplyFromBox(
+      '!<b>@shady90</b></span>[\s]+<div align\="center"><b><a href\="([\S\s]+?)" target\="_blank">!'
+    );
+  }
+
+  private function getReplyFromBox($responsePattern)
+  {
+    $source = $this->curlHelper->getSource($this->cboxUrl.'&sec=main');
+    preg_match($responsePattern, $source, $matches);
+
+    if(sizeof($matches) < 2)
+      return "";
+
+    return $matches[1];
+  }
+
+  private function sendMessageToBox($message)
   {
     $this->curlHelper->postCall($this->cboxUrl.'&sec=submit', [
       'nme' => 'shady90',
@@ -30,24 +58,13 @@ class Rapid8Slave
       'fkey' => '',
       'pic' => '',
       'auth' => '',
-      'pst' => $linkToDownload,
+      'pst' => $message,
       'captme' => '',
       'capword' => '',
       'caphash' => '',
       'aj' => 'x',
       'lp' => '81386',
     ]);
-
-    sleep(self::SECOND_TO_WAIT_FOR_RESPONSE);
-    $source = $this->curlHelper->getSource($this->cboxUrl.'&sec=main');
-
-    $responsePattern = '!<b>@shady90</b></span>[\s]+<div align\="center"><b><a href\="([\S\s]+?)" target\="_blank">!';
-    preg_match($responsePattern, $source, $matches);
-
-    if(sizeof($matches) < 2)
-      return "";
-
-    return $matches[1];
   }
 
 }
--- tests/Rapid8SlaveTest.php
+++ tests/Rapid8SlaveTest.php
@@ -15,6 +15,9 @@ class Rapid8SlaveTest extends PHPUnit_Framework_TestCase
   function testRapid8SlaveRecognizesProviders()
   {
     $link = 'http://uploaded.net/file/wmrhn8gt/sole24ore20150104.pdf';
-    $this->assertTrue($this->r8s->isProviderSupported($link));
+    $this->assertTrue($this->r8s->isLinkSupported($link));
+
+    $link = 'http://rapidgator.net/file/1380cb57ed533790984e3ba07f9ad669/test.html';
+    $this->assertTrue($this->r8s->isLinkSupported($link));
   }
 }
